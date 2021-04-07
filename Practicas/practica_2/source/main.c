7d103dd35352ff4f6ab3ba5349a7b94cc6cc68f6
Dercont <armando.almengor@utp.ac.pa>
2021-04-07 02:24:05 +0200

    v1.3

--- Practicas/practica_2/source/main.c
+++ Practicas/practica_2/source/main.c
@@ -14,8 +14,6 @@ int main(int argc, char **argv)
 	//Initialize console on top screen. Using NULL as the second argument tells the console library to use the internal console structure as current one
 	consoleInit(GFX_TOP, NULL);
 
-	u32 kDownOld = 0; //In these variables there will be information about keys detected in the previous frame
-
 	//Touch Counter
 	int times = 0;
 
@@ -32,12 +30,9 @@ int main(int argc, char **argv)
 		u32 kDown = hidKeysDown();
 
 		if (kDown & KEY_A)
-			break; // break in order to return to hbmenu
+			times = 0;
 		if (kDown & KEY_TOUCH)
 			times++;
-
-		//Do the keys printing only if keys have changed
-		if (kDown != kDownOld)
 
 		{
 			//Clear console
@@ -47,9 +42,6 @@ int main(int argc, char **argv)
 			printf("\x1b[1;1HPress A to restart counter.");
 			printf("\x1b[2;1HTouched Times: %d", (int)times);
 		}
-
-		//Set keys old values for the next frame
-		kDownOld = kDown;
 
 		// Flush and swap framebuffers
 		gfxFlushBuffers();
