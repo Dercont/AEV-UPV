df9860d2d3b0584cbebe618e1bbcf402b768ba08
Dercont <armando.almengor@utp.ac.pa>
2021-04-07 18:30:36 +0200

    v2

--- Practicas/practica_2/source/main.c
+++ Practicas/practica_2/source/main.c
@@ -1,5 +1,6 @@
 /*
-Se propone hacer una aplicación para 3DS que cuente el número de clics que el usuario hace en la pantalla táctil durante 3 segundos (aproximadamente) y vaya mostrando el contador en la pantalla superior. Al pulsar la tecla ‘A’ el proceso se debe reiniciar.
+	Based on the work of Circle Pad example made by Aurelio Mannara for libctru
+	by Alexander Almengor
 */
 
 #include <3ds.h>
@@ -8,16 +9,20 @@ Se propone hacer una aplicación para 3DS que cuente el número de clics que el 
 
 int main(int argc, char **argv)
 {
-	//Timer
-
 	// Initialize services
 	gfxInitDefault();
 
 	//Initialize console on top screen. Using NULL as the second argument tells the console library to use the internal console structure as current one
 	consoleInit(GFX_TOP, NULL);
 
-	//Touch Counter
-	int times = 0;
+	//Click Counter and timer
+	int counter = 0, timer = 3;
+
+	// Timer
+	time_t current_epoch_time, next_stop;
+	time(&current_epoch_time);
+	time(&next_stop);
+	double diff_t = difftime(next_stop, current_epoch_time);
 
 	// Main loop
 	while (aptMainLoop())
@@ -28,20 +33,40 @@ int main(int argc, char **argv)
 		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
 		u32 kDown = hidKeysDown();
 
-		//Labels
-		printf("\x1b[1;1HPress A to restart counter.");
-		printf("\x1b[2;1HTouched Times: %d", (int)times);
+		//Instrucctions
+		printf("\x1b[5;9HCounter Clicks App 3DS");
+		printf("\x1b[6;9HHow many clicks can you make in 3 seconds?");
+		printf("\x1b[7;9HPress A to Start/Restart");
+		printf("\x1b[8;9HPress Start to exit");
 
 		//Timer
+		time(&current_epoch_time);						  // Get current EPOCH time from System
+		diff_t = difftime(next_stop, current_epoch_time); // Time Difference
+		if (diff_t >= 0 && diff_t <= 3)					  // less than 3 seconds and more than 3 the timer
+			timer = diff_t;
+		printf("\x1b[9;9HTimer: %d", (int)timer);
+		if (diff_t < 0) // if the difft is not lees than 3 secs and more than 3
+			counter = 0;
+		printf("\x1b[10;9HClicks: %d", (int)counter);
+		printf("\x1b[2;9HDiff = %f", (double)diff_t);
+
+		// Keys validatation
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
 
 		if (kDown & KEY_A)
-			//Clear console
-			consoleClear();
+		{
+			// Get Next_Stop for timer
+			next_stop = current_epoch_time + 3;
+			consoleClear(); //Clear console
+		}
 
+		// Touch Screen Validation
+		// Do if the touch screen has been touched clicks +1
 		if (kDown & KEY_TOUCH)
 		{
-			if ()
-				times++;
+			if (diff_t > 0 && diff_t <= 3)
+				counter++;
 		}
 
 		// Flush and swap framebuffers
